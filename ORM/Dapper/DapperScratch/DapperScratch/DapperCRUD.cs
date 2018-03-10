d2c135416704a67fa1c66aa3c31013d39313a513
jackyHomePC <liujinjacky2006@hotmail.com>
2018-03-11 00:08:32 +0800

    add Dapper insert (bulk) code

---
+++ ORM/Dapper/DapperScratch/DapperScratch/DapperCRUD.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DapperScratch
+{
+    public class DapperCRUD
+    {
+        IDbConnection connection = new SqlConnection("Data Source=.;Initial Catalog=DapperLab;Integrated Security=True;MultipleActiveResultSets=True");
+
+        public int SingleInsert(string userName, string email, string address)
+        {
+            var affectedRows = connection.Execute("insert into Users values (@UserName, @Email, @Address)",
+                                           new { UserName = userName, Email = email, Address = address });
+
+            return affectedRows;
+        }
+
+        public int BulkInsert(IEnumerable<Users> newUsers)
+        {
+            return connection.Execute("insert into Users values (@UserName, @Email, @Address)", newUsers);
+        }
+
+        public IEnumerable<Users> QueryByUserName(string userName)
+        {
+            var result = connection.Query<Users>("select * from Users where UserName=@UserName",
+                new { UserName = userName });
+
+            return result;
+        }
+
+        public IEnumerable<Users> SearchByUserName(string userName)
+        {
+            var result = connection.Query<Users>("select * from Users where UserName like @UserName",
+                new { UserName = userName + "%" });
+
+            return result;
+        }
+    }
+
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ ORM/Dapper/DapperScratch/Test.DapperScratch/TestDapperCRUD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using DapperScratch;
+using Xunit;
+
+namespace Test.DapperScratch
+{
+    public class TestDapperCRUD
+    {
+        private DapperCRUD dapperCrud;
+        public TestDapperCRUD()
+        {
+            dapperCrud = new DapperCRUD();
+        }
+
+        [Fact]
+        public void TestSingleInsert()
+        {
+            var testUserName = $"JeremyLiu_{DateTime.Now.Ticks}";
+            var rows = dapperCrud.SingleInsert(testUserName, "dapper@test.com", "dummy address");
+            Assert.Equal(1, rows);
+
+            var insertResult = dapperCrud.QueryByUserName(testUserName).SingleOrDefault();
+            Assert.Equal(testUserName, insertResult.UserName);
+        }
+
+        [Fact]
+        public void TestBulkInsert()
+        {
+            var testUserName = $"JeremyLiu_{DateTime.Now.Ticks}";
+            var newUsers = Enumerable.Range(0, 10).Select(i => new Users
+            {
+                UserName = $"{testUserName}_{i}",
+                Email = $"dapper@test.com_{i}",
+                Address = $"dummy address-{i}"
+            });
+
+            dapperCrud.BulkInsert(newUsers);
+
+            var bulkInsertResults = dapperCrud.SearchByUserName(testUserName).ToArray();
+            Assert.Equal(10, bulkInsertResults.Count());
+            Assert.True(bulkInsertResults.All(u => u.UserName.Contains(testUserName)));
+        }
+    }
+}
