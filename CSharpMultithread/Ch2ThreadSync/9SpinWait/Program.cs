fe0c7a1a6c750006dc576a44470cc489da6bce7e
JeremyLiuCompany <liujinjacky2006@hotmail.com>
2018-05-24 16:43:48 +0800

    add the rest of charpter2 demo codes

---
+++ CSharpMultithread/Ch2ThreadSync/9SpinWait/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _9SpinWait
+{
+    class Program
+    {
+        static volatile bool _isCompleted = false;
+
+        static void UserModeWait()
+        {
+            while (!_isCompleted)
+            {
+                Console.Write(".");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Waiting is complete");
+        }
+
+        static void HybridSpinWait()
+        {
+            var w = new SpinWait();
+            while (!_isCompleted)
+            {
+                w.SpinOnce();
+                Console.WriteLine(w.NextSpinWillYield);
+            }
+
+            Console.WriteLine("Waiting is complete....");
+        }
+
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(UserModeWait);
+            var t2 = new Thread(HybridSpinWait);
+
+            Console.WriteLine("Running user mode waiting");
+            t1.Start();
+
+            Thread.Sleep(20);
+            _isCompleted = true;
+
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            _isCompleted = false;
+
+            Console.WriteLine("Running hybrid SpinWait contruct waiting");
+            t2.Start();
+
+            Thread.Sleep(5);
+            _isCompleted = true;
+
+            Console.WriteLine("done...");
+            Console.ReadKey();
+        }
+    }
+}
